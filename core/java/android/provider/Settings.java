bac5d8ce41f006e7daa30d5d63a8b4c1a0bdc072
Yashar Ahmedizadeh <yashade2001@gmail.com>
2014-12-01 22:08:24 +0200

    Add option to disable search bar in recents [2/2]
    Coded from scratch by yashade2001.
    Firstly written for Project D.I.S.C.O. ROM.
    Thanks to TheMrcool212 for his help.
    
    Signed-off-by: Yashar Ahmedizadeh <yashade2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3265,6 +3265,12 @@ public final class Settings {
         public static final String APP_CIRCLE_BAR_SHOW_TRIGGER = "app_circle_bar_show_trigger";
 
         /**
+         * Recents Show/Hide Search Bar
+         * @hide
+         */
+        public static final String RECENTS_SHOW_HIDE_SEARCH_BAR = "recents_show_hide_search_bar";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
